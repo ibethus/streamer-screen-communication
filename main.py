4202f153053907c3e0c803a0ae9d89f12e9a6785
Ivan <ivan.bethus@gmail.com>
2023-12-16 17:34:03 +0100

    Add delay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import time
 import serial
 
 def print_data_tu_usb(data):
@@ -9,18 +10,27 @@ def print_data_tu_usb(data):
     s.close()
 
 def main():
-    alsa = open("/proc/asound/card0/pcm0p/sub0/hw_params")    
-    if alsa is not None:
-        depth = ""
-        rate = ""
-        for alsa_line in alsa.readlines():            
-            if alsa_line.startswith("format"):
-                depth = re.search("\d{2}", alsa_line).group()
-                depth = "Bit depth {depth} bit".format(depth=depth)
-            if alsa_line.startswith("rate"):
-                rate = int(alsa_line.split(":")[1].strip().split(" ")[0].strip())/1000
-                rate = "Sample rate {rate}kHz".format(rate=rate)
-        print_data_tu_usb("{depth}\n{rate}".format(depth=depth, rate=rate))
+    previous_rate = ""
+    previous_depth = ""
+    while True:
+        alsa = open("/proc/asound/card0/pcm0p/sub0/hw_params")    
+        if alsa is not None:
+            depth = ""
+            rate = ""
+            for alsa_line in alsa.readlines():  
+                if alsa_line.startswith("format"):
+                    depth = re.search("\d{2}", alsa_line).group()
+                    depth = "Bit depth {depth} bit".format(depth=depth)
+                if alsa_line.startswith("rate"):
+                    rate = int(alsa_line.split(":")[1].strip().split(" ")[0].strip())/1000
+                    rate = "Sample rate {rate}kHz".format(rate=rate)
+            if rate != previous_rate or depth != previous_depth:
+                print_data_tu_usb("{depth}\n{rate}".format(depth=depth, rate=rate))
+                previous_depth = depth
+                previous_rate = rate
+            else:
+                print("nothing changed")                
+        time.sleep(10)
 
 
 if __name__ == "__main__":
